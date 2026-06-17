1943b6379a4534b17a345aa425868961111c663f
Belka-letyaga <alexsten1811@gmail.com>
2026-06-17 19:02:10 +0500

    g

--- internal/handler/logout_handler.go
+++ internal/handler/logout_handler.go
@@ -1,12 +1,9 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func LogoutHandler(c *gin.Context) {
-	c.SetCookie("tokenn", " ", -1, "/", "", false, true)
-	c.Redirect(http.StatusSeeOther, "/login")
+	c.SetCookie("token", " ", -1, "/", "", false, true)
 }
