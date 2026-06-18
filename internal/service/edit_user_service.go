339959e84ec6a547d996561e1d8e5d556e2cc13f
Belka-letyaga <alexsten1811@gmail.com>
2026-06-18 16:48:14 +0500

    g

--- internal/service/edit_user_service.go
+++ internal/service/edit_user_service.go
@@ -3,8 +3,14 @@ package service
 import (
 	"github.com/BelanAlexandr/back/internal/models"
 	"github.com/BelanAlexandr/back/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func EditUserService(creator_id int, user models.User) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
 	return repository.EditUserRepo(creator_id, user)
 }
